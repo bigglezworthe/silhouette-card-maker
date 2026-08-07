b30f884375b71896a806607e7599df36d3bc770f
bigglezworthe <vinceofalltrades@gmail.com>
2026-08-06 20:49:09 -0700

    restore test_utilities

---
+++ test/test_utilities.py
@@ -0,0 +1,8 @@
+# Should be phased out eventually. Here to satisfy PR submission tests.
+
+from test.test_src_draw import *
+from test.test_src_images import *
+from test.test_src_layouts import *
+from test.test_src_offset import *
+from test.test_src_paths import *
+from test.test_src_pdf import *
